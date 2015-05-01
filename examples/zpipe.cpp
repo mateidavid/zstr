d39b55d7d080292ac91eb265fb3a9b3a3b1c79cb
Matei David <matei@cs.toronto.edu>
2015-05-01 16:32:23 -0400

    simpler examples

--- examples/zpipe.cpp
+++ examples/zpipe.cpp
@@ -2,53 +2,33 @@
 #include <string>
 #include "zstr.hpp"
 
-int main(int argc, char * argv[])
+int main()
 {
     //
-    // turn on error reporting
+    // Create explicit zstr::streambuf feeding off the streambuf of std::cin.
+    // This syntax allows for setting the buffer size and the auto-detect option.
     //
-    std::cin.exceptions(std::ifstream::badbit);
-    std::cout.exceptions(std::ifstream::badbit);
+    zstr::streambuf zsbuf(std::cin.rdbuf(), 1<<16, true);
     //
-    // construct input zstream
-    // (only 1 of 3 possible ways will be used)
+    // Create an std::istream wrapper for the zstr::streambuf.
+    // NOTE: A zstr::istream constructed with a zstr::streambuf parameter would decompress twice.
     //
-    std::istream * is_p = nullptr;
-    zstr::streambuf * zsbuf_p = nullptr;
-    if (argc > 1 and std::string(argv[1]) == "-1")
-    {
-        std::clog << "construct zstr::streambuf from std::streambuf, wrap it in std::istream" << std::endl;
-        zsbuf_p = new zstr::streambuf(std::cin.rdbuf());
-        is_p = new std::istream(zsbuf_p);
-    }
-    else if (argc > 1 and std::string(argv[1]) == "-2")
-    {
-        std::clog << "construct zstr::istream from std::streambuf, convert it to std::istream" << std::endl;
-        is_p = new zstr::istream(std::cin.rdbuf());
-    }
-    else
-    {
-        std::clog << "construct zstr::istream from std::istream, convert it to std::istream" << std::endl;
-        is_p = new zstr::istream(std::cin);
-    }
-    assert(is_p);
-    is_p->exceptions(std::ifstream::badbit);
+    std::istream is(&zsbuf);
     //
-    // main loop
+    // Turn on error reporting (otherwise, zstream exceptions are hidden).
+    //
+    is.exceptions(std::ios_base::badbit);
+    //
+    // Main loop
     //
     const std::streamsize buff_size = 1 << 16;
     char * buff = new char [buff_size];
     while (true)
     {
-        is_p->read(buff, buff_size);
-        std::streamsize cnt = is_p->gcount();
+        is.read(buff, buff_size);
+        std::streamsize cnt = is.gcount();
         if (cnt == 0) break;
         std::cout.write(buff, cnt);
     }
     delete [] buff;
-    //
-    // deallocate std::istream and zstr::streambuf (if allocated)
-    //
-    delete is_p;
-    if (zsbuf_p) delete zsbuf_p;
 }
