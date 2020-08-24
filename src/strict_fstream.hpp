ff26915ac6c70bafe703286dda44a36d9985d50a
ferdymercury <ferdymercury@users.noreply.github.com>
2020-08-24 07:00:11 -0400

    Only trim buffer if there is a \0 in it
    Fixes https://github.com/mateidavid/zstr/issues/20

--- src/strict_fstream.hpp
+++ src/strict_fstream.hpp
@@ -39,7 +39,10 @@ static std::string strerror()
     std::string tmp(p, std::strlen(p));
     std::swap(buff, tmp);
 #endif
-    buff.resize(buff.find('\0'));
+    if(buff.find('\0') != std::string::npos)
+    {
+        buff.resize(buff.find('\0'));
+    }
     return buff;
 }
 
