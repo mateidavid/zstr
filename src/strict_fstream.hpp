ac0bdd784cd9f4d00ffe40a41ede94602fef08f0
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-11-06 15:32:37 +0100

    forgot that musl defines _GNU_SOURCE even if it's a lie

--- src/strict_fstream.hpp
+++ src/strict_fstream.hpp
@@ -37,7 +37,7 @@ static std::string strerror()
         buff = "Unknown error";
     }
     return buff;
-#elif (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 || defined(__APPLE__) || defined(BROKEN_GNU_STRERROR_R)) && ! _GNU_SOURCE
+#elif ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600 || defined(__APPLE__)) && ! _GNU_SOURCE) || defined(BROKEN_GNU_STRERROR_R)
 // XSI-compliant strerror_r()
     if (strerror_r(errno, buff.data(), buff.size()) == 0) {
         buff.resize(buff.find('\0'));
